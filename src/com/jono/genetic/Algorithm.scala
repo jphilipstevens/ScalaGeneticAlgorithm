0a4628085e728fabeb1583a1cdb19f22ba3a56ac
jphilipstevens <jstevens1984@gmail.com>
2013-04-24 21:23:23 -0400

    Laying more framework and commenting as I understand this

--- src/com/jono/genetic/Algorithm.scala
+++ src/com/jono/genetic/Algorithm.scala
@@ -1,5 +1,7 @@
 package com.jono.genetic
 
+import com.jono.genetic.selection.SelectionMechanism
+
 /**
  * This is a Genetic Algorithm. It can run per iteration and return to the caller or run until it solves to problem
  */
@@ -7,6 +9,9 @@ class Algorithm(initialPopulation: List[Subject]) {
 
   // a probability used in crossover
   var crossoverPob: Double
+
+  // selection algorithm used to do crossovers
+  var selectionMechanism: SelectionMechanism
 
   def getGeneration(population: List[Subject]): List[Subject] = {
 
---
+++ src/com/jono/genetic/Mutator.scala
@@ -0,0 +1,8 @@
+package com.jono.genetic
+
+/**
+ * This will perform random mutations on Subjects as does happen in real life
+ */
+class Mutator {
+
+}
